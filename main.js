3767283581f771234d0d82102b2a30062b242131
agasan371119 <yna77351unichi@icloud.com>
2020-06-01 19:40:58 +0900

    追加

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-
+const SCREEN_SIZE_W = 256;
+const SCREEN_SIZE_H = 224;
 
 let can = document.getElementById("can");
 let con = an.getContext("2d");
+can.width = SCREEN_SIZE_W;
+can.height = SCREEN_SIZE_H;
