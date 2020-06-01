6e7da7b370ed4636353e1f1115f376889b360270
agasan371119 <yna77351unichi@icloud.com>
2020-06-01 19:57:16 +0900

    変更

--- main.js
+++ main.js
@@ -3,5 +3,9 @@ const SCREEN_SIZE_H = 224;
 
 let can = document.getElementById("can");
 let con = an.getContext("2d");
+
 can.width = SCREEN_SIZE_W;
 can.height = SCREEN_SIZE_H;
+
+con.fillStyle = "#66AAFF";
+con.fillRect(0, 0, SCREEN_SIZE_W, SCREEN_SIZE_H);
