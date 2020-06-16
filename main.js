654690598e0f33b3ae4ade6558499c14ee523c45
agasan371119 <yna77351unichi@icloud.com>
2020-06-16 21:17:58 +0900

    画面設定完了

--- main.js
+++ main.js
@@ -3,12 +3,15 @@ const SCREEN_W = 180;
 const SCREEN_H = 320;
 
 //キャンバスサイズ
-const CANVAN_W = SCREEN_W * 2;
-const CANVAN_H = SCREEN_H * 2;
+const CANVAS_W = SCREEN_W * 2;
+const CANVAS_H = SCREEN_H * 2;
 
 //フィールドサイズ
 const FIELD_W = SCREEN_W * 2;
-const FIELD_W = SCREEN_H * 2;
+const FIELD_H = SCREEN_H * 2;
+
+//星の数
+const STAR_MAX = 300;
 
 //キャンバス
 let can = document.getElementById("can");
@@ -16,3 +19,36 @@ let con = can.getContext("2d");
 can.width = CANVAS_W;
 can.height = CANVAS_H;
 
+function rand(min,max) {
+  return Math.floor(Math.random() * (max-min+1)) + min;
+}
+
+//星クラス
+
+class Star {
+  constructor() {
+    this.x = rand(0, FIELD_W) << 8;
+    this.y = rand(0, FIELD_H) << 8;
+    this.vx = 0;
+    this.vy = rand (30, 200);
+    this.sz = rand (1, 2);
+  }
+
+  draw() {
+    con.fillStyle = rand(0, 2) != 0?"66f":"#8af";
+    con.fillRect(this.x >> 8, this.y >> 8, this.sz, this.sz);
+
+  }
+
+  update() {
+    this.x += this.vx;
+    this.y += this.vy;
+    if(this.y > FIELD_H <<8) {
+      this.y = 0;
+      this.x = rand(0,FIELD_W) <<8;
+    } 
+  }
+}
+let star = [];
+for(let i = 0; i < STAR_MAX; i++)star[i] = new Star();
+for(let i = 0; i < STAR_MAX; i++)star[i]. draw();
