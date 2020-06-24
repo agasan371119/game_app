eaa131974435aa6b18e08028f7b41980915fea38
agasan371119 <yna77351unichi@icloud.com>
2020-06-24 11:25:15 +0900

    分割修正１

--- main.js
+++ main.js
@@ -45,133 +45,14 @@ let star = [];
 //キーボードの状態
 let key = [];
 
-//キーボードが押された時
-document.onkeydown = function(e){
-  key[ e.keyCode] = true;
-}
-
-//キーボードが話された時
-document.onkeyup = function(e) {
-  key[ e.keyCode] = false;
-}
-
-//キャラクターのベースクラス
-class CharaBase {
-  constructor (snum, x, y, vx, vy) {
-    this.sn   = snum;
-    this.x    =  x;
-    this.y    =  y;
-    this.vx   = vx;
-    this.vy   = vy;
-    this.kill = false;
-  }
-  update () {
-    this.x += this.vx;
-    this.y += this.vy;
-
-    if(this.x < 0 || this.x > FIELD_W << 8 || this.y < 0 
-      || this.y > FIELD_H << 8)this.kill = true; {
-
-    }
-  }
-  draw() {
-    drawSprite(this.sn, this.x, this.y);
-  }
-}
-
-
-//敵クラス
-class Teki extends CharaBase {
-
-  constructor (snum, x, y, vx, vy) {
-    super(snum, x, y, vx, vy);
-  }
-
-  update() {
-    super.update();
-  }
-
-  draw() {
-    super.draw();
-  }
-}
 
 let teki = [
   new Teki(39, 200 << 8, 200 << 8, 0, 0)
 ];
 
-//弾クラス
-class Tama extends CharaBase {
-
-  constructor (x, y, vx, vy) {
-    super(5, x, y, vx, vy);
-    //
-    //
-  }
-  update () {
-    super.update();
-  }
-  draw() {
-    super.draw();
-  }
-}
 
 let tama = [];
 
-//自機クラス
-class Jiki {
-
-  constructor() {
-    this.x = (FIELD_W/2)<<8;
-    this.y = (FIELD_H/2)<<8;
-    this.speed  = 512;
-    this.anime  = 0;
-    this.reload = 0;
-    this.relo2  = 0;
-  }
-  //自機の移動
-  update() {
-    if (key[32] && this.reload == 0) {
-      tama.push(new Tama(this.x+(4 << 8), this.y-(10 << 8),   0, -2000));
-      tama.push(new Tama(this.x-(4 << 8), this.y-(10 << 8),   0, -2000));
-      tama.push(new Tama(this.x+(8 << 8), this.y-(10 << 8),  80, -2000));
-      tama.push(new Tama(this.x-(8 << 8), this.y-(10 << 8), -80, -2000));
-
-      this.reload = 4;
-
-      if (++this.relo2 == 4) {
-        this.reload = 20;
-        this.relo2  =  0;
-      }
-    }
-    if (!key[32] )this.reload = this.relo2 = 0;
-
-    if(this.reload > 0) this.reload--;
-
-    if(key[37] && this.x > this.speed) {
-
-      this.x -= this.speed;
-      if(this.anime >- 8)this.anime--;
-
-    } else if( key[39] && this.x <= (FIELD_W << 8)-this.speed) {
-
-      this.x += this.speed;
-      if(this.anime < 8)this.anime++;
-
-    } else {
-      if(this.anime > 0)this.anime--;
-      if(this.anime < 0)this.anime++;
-    }
-
-    if ( key[38] && this.y > this.speed )this.y -= this.speed; 
-    if ( key[40] && this.y <= (FIELD_H << 8)-this.speed)this.y += this.speed; 
-
-  }
-  //描画
-  draw() {
-    drawSprite(2 + (this.anime >> 2), this.x, this.y);
-  }
-}
 let jiki = new Jiki();
 
 
@@ -287,60 +168,7 @@ let sprite =[
 
 ];
 
-//スプライトを描画する
-function drawSprite(snum, x, y) {
-  let sx = sprite[snum].x;
-  let sy = sprite[snum].y;
-  let sw = sprite[snum].w;
-  let sh = sprite[snum].h;
 
-  let px = (x >> 8) - sw/2;
-  let py = (y >> 8) - sh/2;
-
-  if(px + sw < camera_x || px >= camera_x + SCREEN_W 
-    || py + sh < camera_y || py >= camera_y + SCREEN_H)return;
-
-
-  vcon.drawImage(spriteImage, sx, sy, sw, sh, px, py, sw, sh);
-}
-
-//整数のランダムを作る
-function rand(min,max) {
-  return Math.floor(Math.random() * (max-min+1)) + min;
-}
-
-//星クラス
-
-class Star {
-  constructor() {
-    this.x = rand(0, FIELD_W) << 8;
-    this.y = rand(0, FIELD_H) << 8;
-    this.vx = 0;
-    this.vy = rand (30, 200);
-    this.sz = rand (1, 2);
-  }
-
-  draw() {
-    let x = this.x >> 8;
-    let y = this.y >> 8;
-
-    if(x < camera_x || x >= camera_x + SCREEN_W 
-       || y < camera_y || y >= camera_y + SCREEN_H)return;
-
-    vcon.fillStyle = rand(0, 2) != 0?"66f":"#8af";
-    vcon.fillRect(x, y, this.sz, this.sz);
-
-  }
-
-  update() {
-    this.x += this.vx;
-    this.y += this.vy;
-    if(this.y > FIELD_H <<8) {
-      this.y = 0;
-      this.x = rand(0,FIELD_W) <<8;
-    } 
-  }
-}
 
 //ゲーム初期化
 function gameInit() {
